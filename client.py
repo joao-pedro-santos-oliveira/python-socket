cdd51dcb1a689f9b597d83d8f1eff662122bbf15
João Santos <joaopedro.santosoliveira2003@gmail.com>
2024-01-28 11:16:19 -0300

    fixing message format

--- client.py
+++ client.py
@@ -1,32 +1,43 @@
 from socket import *
 import threading
+import re
 
 host = "localhost"
 port = 11311
 BUFFER_SIZE = 1024
 SAIR_SALA = "bye"
 CONECTAR_SALA = "hi, meu nome eh"
-isOnline = False
 serverAddress = (host, port)
 sock = socket(AF_INET, SOCK_DGRAM)
 
-
-
+def extrair_nome_usuario(input_string):
+    # Expressão regular para extrair o nome do usuário
+    pattern = r'<(.*?)>'
+    match = re.search(pattern, input_string)
+    
+    if match:
+        # Retorna o primeiro grupo de captura que corresponde ao nome do usuário
+        return match.group(1)
+    else:
+        return None
 
 def main():
     mensagem = input("Para se conectar, escreva da seguinte forma 'hi, meu nome eh<seu_nome>'")
 
     if(mensagem.startswith(CONECTAR_SALA)):
         sock.sendto(mensagem.encode(), serverAddress)
-
-        sock.sendto("entrou na sala!".encode('utf-8'), serverAddress)
+        user_name = extrair_nome_usuario(mensagem)
         print("Conectado com sucesso!")
 
-        thread_envio = threading.Thread(target=enviar_mensagens, args=())
+        thread_envio = threading.Thread(target=enviar_mensagens, args=[user_name])
         thread_recebimento = threading.Thread(target=receber_mensagens, args=())
         
         thread_envio.start()
         thread_recebimento.start()
+
+    else:
+        print("Usuário não conectado!")
+        main()
 
 
 
@@ -34,18 +45,17 @@ def receber_mensagens():
     while True:
         clientMessage, clientAddress = sock.recvfrom(BUFFER_SIZE)
         clientMessageDecoded = clientMessage.decode()
-        print(clientAddress, clientMessageDecoded)
+        print(clientMessageDecoded)
 
-def enviar_mensagens():
+def enviar_mensagens(user_name):
     while True:
         mensagem = input('Digite: ')
         if mensagem == SAIR_SALA:
             break
-        elif mensagem.startswith(CONECTAR_SALA):
-            sock.sendto(mensagem.encode(), serverAddress)
         else:
             with open("mensagens.txt", "w") as arquivo:
-                arquivo.write(mensagem)
+                full_message = f'<{user_name}>:{mensagem}'
+                arquivo.write(full_message)
 
             with open("mensagens.txt", "r") as arquivo_leitura:
                 dados = arquivo_leitura.read(BUFFER_SIZE)
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from socket import *
+import re
 
 host ="localhost"
 port = 11311
@@ -14,22 +15,29 @@ conected_clients = set()
 while True:
     clientMessage, clientAddress = sock.recvfrom(BUFFER_SIZE)
     clientMessageDecoded = clientMessage.decode()
+
+    user_ip = clientAddress[0]
+    user_port = clientAddress[1]
+    palavras = clientMessageDecoded.split()
     if(clientMessageDecoded.startswith(CONECTAR_SALA)):
         conected_clients.add(clientAddress)
-        print(conected_clients)
 
     elif(clientMessageDecoded.startswith(SAIR_SALA)):
         if(clientAddress in conected_clients):
             conected_clients.remove(clientAddress)
-        else:
-            None
     else:
-        if(clientAddress in conected_clients):
-            for client in conected_clients:
-                sock.sendto(clientMessageDecoded.encode(), client)
+        with open("python-socket/mensagens.txt", "w") as arquivo:
+            full_message = f'<{user_ip}>:<{user_port}>/~{clientMessageDecoded}'
+            arquivo.write(full_message)
 
-        else:
-            None
+        with open("python-socket/mensagens.txt", "r") as arquivo_read:
+            dados = arquivo_read.read(BUFFER_SIZE)
+            if(clientAddress in conected_clients):
+                for client in conected_clients:
+                    sock.sendto(dados.encode(), client)
+                    
+
+
         
 
         
