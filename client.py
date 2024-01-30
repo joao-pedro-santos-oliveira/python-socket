91f72fa921f2ff30f036684dadb16fad47bbf921
João Santos <joaopedro.santosoliveira2003@gmail.com>
2024-01-29 23:07:08 -0300

    adding comments to code

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
+#importação de bibliotecas
 from socket import *
 import threading
 import re
 
+#inicialização de variáveis
 host = "localhost"
 port = 11311
 BUFFER_SIZE = 1024
@@ -23,44 +25,57 @@ def extract_name(input_string):
 def main():
     mensagem = input("Para se conectar, escreva da seguinte forma 'hi, meu nome eh<seu_nome>'")
 
+    #checando se o usuário digitou o comando para se conectar
     if(mensagem.startswith(CONECTAR_SALA)):
+        #enviando mensagem para o servidor para conectar o client
         sock.sendto(mensagem.encode(), serverAddress)
         user_name = extract_name(mensagem)
         print("Conectado com sucesso!")
 
+        #inicializando as threads para enviar e receber mensagens
         thread_envio = threading.Thread(target=enviar_mensagens, args=[user_name])
         thread_recebimento = threading.Thread(target=receber_mensagens, args=())
-        
         thread_envio.start()
         thread_recebimento.start()
 
     else:
+        #caso o usuário tente enviar uma mensagem sem estar conectado
         print("Usuário não conectado!")
         main()
 
 def receber_mensagens():
+    #função para receber a mensagem do servidor e exibir na tela
     while True:
         clientMessage, clientAddress = sock.recvfrom(BUFFER_SIZE)
         clientMessageDecoded = clientMessage.decode()
         print(clientMessageDecoded)
 
 def enviar_mensagens(user_name):
+    #função para adicionar a mensagem num arquivo txt e enviar para o servidor
     while True:
         mensagem = input()
+        #checando se o usuario deseja sair da sala
         if mensagem == SAIR_SALA:
+            #caso queira, passamos a informação para o servidor e o usuário é desconectado, não podendo enviar mais mensagens
             full_message = f'<{user_name}>{mensagem}'
             sock.sendto(full_message.encode(), serverAddress)
             print("Usuário desconectado!")
+            #return False para encerrar a função de enviar mensagem, já que o usuário se desconectou
             return False
         else:
+            #criando/abrindo o arquivo txt em modo escrita e inserindo a mensagem
             with open("mensagens.txt", "w") as arquivo:
                 full_message = f'<{user_name}>:{mensagem}'
                 arquivo.write(full_message)
 
+            #abrindo o arquivo em modo leitura
             with open("mensagens.txt", "r") as arquivo_leitura:
+                #a variavel recebe os dados do buffer
                 dados = arquivo_leitura.read(BUFFER_SIZE)
+                #enquanto houverem dados a serem enviados, ele continuara enviando
                 while dados:
                     sock.sendto(dados.encode(), serverAddress)
+                    #caso não seja possivel enviar todos os bits de uma vez, variavel dados recebe o que faltou
                     dados = arquivo_leitura.read(BUFFER_SIZE)
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+#importação de bibliotecas
 from socket import *
 import re
 from datetime import *
 
+#inicialização de variáveis
 host ="localhost"
 port = 11311
 BUFFER_SIZE =1024
@@ -9,8 +11,11 @@ SAIR_SALA = "bye"
 CONECTAR_SALA = "hi, meu nome eh"
 addr = (host, port)
 sock = socket(AF_INET, SOCK_DGRAM)
+
+#fazendo um bind do endereço do servidor para que ele permaneça sempre o mesmo
 sock.bind(addr)
 
+#para armazenar a lista de clients conectados
 conected_clients = set()
 
 def extract(input_string, type):
@@ -23,26 +28,30 @@ def extract(input_string, type):
         match = re.search(pattern, input_string)
     
     if match:
-        # Retorna o primeiro grupo de captura que corresponde ao nome do usuário
         return match.group(1)
     else:
         return None
 
 def connect_client(clientAddress, user_name):
+    #função para conectar os clients e adicionar o seu endereço na lista de clients conectados
     conected_clients.add(clientAddress)
     for client in conected_clients:
+        #avisando a todos os clients que um novo client foi conectado
         connection_message = f'{user_name} entrou na sala!'
         sock.sendto(connection_message.encode(), client)
 
 def remove_client(clientAddress, user_name):
+    #função para remover o client, fazendo com que ele não receba mais as mensagens enviadas
     if(clientAddress in conected_clients):
         removed_message = f'{user_name} saiu do chat!'
         for client in conected_clients:
+            #avisando aos clients que um client foi desconectado
             if(client != clientAddress):
                 sock.sendto(removed_message.encode(), client)
         conected_clients.remove(clientAddress)
 
 def broadcast(user_ip, user_port, clientMessageDecoded, timestamp, clientAddress):
+    #função para retransmitir a mensagem recebida de um client para todos os clients conectados
     with open("python-socket/mensagens.txt", "w") as arquivo:
         full_message = f'<{user_ip}>:<{user_port}>/~{clientMessageDecoded} {timestamp}'
         arquivo.write(full_message)
@@ -55,19 +64,26 @@ def broadcast(user_ip, user_port, clientMessageDecoded, timestamp, clientAddress
 
 def main():
     while True:
+        #recebendo o arquivo do client
         clientMessage, clientAddress = sock.recvfrom(BUFFER_SIZE)
         clientMessageDecoded = clientMessage.decode()
+        #identificando o IP do client e a porta
         user_ip = clientAddress[0]
         user_port = clientAddress[1]
+        #pegando a data/hora o horario da mensagem e formatando
         timestamp = datetime.now().strftime('%H:%M:%S %Y-%m-%d')
+        #extraindo o nome do usuário e a mensagem em duas variáveis diferentes
         user_name = extract(clientMessageDecoded, "name")
         message = extract(clientMessageDecoded, "message")
 
+        #checando se o client deseja se conectar
         if(clientMessageDecoded.startswith(CONECTAR_SALA)):
             connect_client(clientAddress, user_name)
-            
+
+        #checando se o client deseja se desconectar
         elif(message.startswith(SAIR_SALA)):
             remove_client(clientAddress, user_name)
+        #enviando a mensagem extraida do arquivo txt para todos os clients
         else:
             broadcast(user_ip, user_port, clientMessageDecoded, timestamp, clientAddress)
 
